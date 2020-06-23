bf02cea9c134c86efaa137b899e8eea14dfed1c5
Andrew Smith <848smith@gmail.com>
2020-06-23 05:01:10 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,29 @@
+class SongsController < ApplicationController
+  def show
+    @song = Song.find(params[:id])
+  end
+  
+  def new
+    @song = Song.new
+  end
+  
+  def create
+    @song = Song.new(song_params(:name))
+    @song.save
+    redirect_to song_path(@song)
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  private
+  
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+end
